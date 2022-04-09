41b9aac4fa4fe9fee1476c93db243df0b6096e4a
kassim0 <kassim_haddad@yahoo.fr>
2022-04-09 13:34:18 +0200

    #1

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src="./logo192.png" alt="logo react" />
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../components/Logo';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Logo />
+        </div>
+    );
+};
+
+export default Home;
